7af32b11d8ee94d09545ba9583e8dba9d63b3513
albapepper <albapepper@gmail.com>
2026-07-03 17:27:55 -0400

    Refine mobile home rail layout

--- src/components/solid/AppRail.tsx
+++ src/components/solid/AppRail.tsx
@@ -210,7 +210,6 @@ export default function AppRail() {
   return (
     <nav
       class="app-rail"
-      classList={{ "app-rail-home-route": isHome() }}
       aria-label="Scoracle navigation"
     >
       <div class="app-rail-brand">
@@ -221,7 +220,6 @@ export default function AppRail() {
           onClick={goHome}
         >
           <BrandMark />
-          <span class="app-rail-title" aria-hidden="true">Scoracle</span>
           <span class="app-rail-tip" aria-hidden="true">Home</span>
         </button>
       </div>
